296024a17b59ac5c89ac0a8e26c6dfe896069142
伊藤尚宏 <itounaohiro@itounaohironoMacBook-Pro.local>
2024-11-10 19:13:29 +0900

    PostFormを追加

--- snsapp/forms.py
+++ snsapp/forms.py
@@ -1,5 +1,10 @@
 from django import forms  
-from .models import Comment  # Comment モデルをインポート
+from .models import Post, Comment  # Comment モデルをインポート
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ['title', 'content', 'category', 'tag'] 
 
 class CommentForm(forms.ModelForm):
     class Meta:
