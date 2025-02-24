02e3a61d29861b4fe1a855e76a7f355f6fe7b441
伊藤尚宏 <itounaohiro@itounaohironoMacBook-Pro.local>
2025-02-24 22:13:47 +0900

    項目追加とデフォルト画像の設定

--- account/forms.py
+++ account/forms.py
@@ -2,7 +2,6 @@ from django import forms
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm, PasswordChangeForm
 from django.contrib.auth import get_user_model # ユーザーモデルを取得するため
 
-
 # ユーザーモデル取得
 User = get_user_model()
 
@@ -20,53 +19,92 @@ class LoginForm(AuthenticationForm):
 
 '''サインアップ用フォーム'''
 class SignupForm(UserCreationForm):
+    JOB_CHOICES = [
+        ('', '未選択'),
+        ('エンジニア', 'エンジニア'),
+        ('デザイナー', 'デザイナー'),
+        ('マーケター', 'マーケター'),
+        ('ディレクター', 'ディレクター'),
+        ('営業', '営業'),
+        ('バックオフィス', 'バックオフィス'),
+        ('CxO', 'CxO'),
+        ('その他', 'その他'),
+    ]
 
-    class Meta:
-        model = User
-        fields = ('email','username')
-
-    def __init__(self, *args, **kwargs):
-
-        super().__init__(*args, **kwargs)
-        for field in self.fields.values():
-            field.widget.attrs['class'] = 'form-control'
-            field.widget.attrs['required'] = '' # 全フィールドを入力必須
-
-            # オートフォーカスとプレースホルダーの設定
-            print(field.label)
-            if field.label == 'メールアドレス':
-                field.widget.attrs['placeholder'] = '***@gmail.com'
-
-'''ユーザー情報更新用フォーム'''
-class UserUpdateForm(forms.ModelForm):
     job_title = forms.ChoiceField(
-        choices=[
-            ('Non_selected', ''),
-            ('Engineer', 'エンジニア'),
-            ('Designer', 'デザイナー'),
-            ('Marketer', 'マーケター'),
-            ('Director', 'ディレクター'),
-            ('Sales', '営業'),
-            ('CxO', 'CxO'),
-            ('Other', 'その他')
-        ],
-        widget=forms.Select(attrs={
+        choices=JOB_CHOICES,
+        required=False,
+        widget=forms.Select(attrs={'class': 'form-control'})
+    )
+    self_introduction = forms.CharField(
+        max_length=1000,
+        required=False,
+        widget=forms.Textarea(attrs={
             'class': 'form-control',
             'placeholder': '自己紹介文を入力してください（1000文字以内）'
-            }),
-        required=False
+        })
+    )
+    self_image = forms.ImageField(
+        required=False,
+        widget=forms.FileInput(attrs={'class': 'form-control-file'})
     )
 
     class Meta:
         model = User
-        fields = ('email', 'username',)
+        fields = ('username', 'email', 'job_title', 'self_introduction', 'self_image')
 
-    # bootstrap4対応
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         for field in self.fields.values():
             field.widget.attrs['class'] = 'form-control'
-            field.widget.attrs['required'] = '' # 全フィールドを入力必須
+            if field.required:
+                field.widget.attrs['required'] = 'required'
+            field.widget.attrs['placeholder'] = field.label  # placeholderにフィールドのラベルを入れる
+
+
+'''ユーザー情報更新用フォーム'''
+class UserUpdateForm(forms.ModelForm):
+    JOB_CHOICES = [
+        ('', '未選択'),
+        ('エンジニア', 'エンジニア'),
+        ('デザイナー', 'デザイナー'),
+        ('マーケター', 'マーケター'),
+        ('ディレクター', 'ディレクター'),
+        ('営業', '営業'),
+        ('バックオフィス', 'バックオフィス'),
+        ('CxO', 'CxO'),
+        ('その他', 'その他'),
+    ]
+
+    job_title = forms.ChoiceField(
+        choices=JOB_CHOICES,
+        required=False,
+        widget=forms.Select(attrs={'class': 'form-control'})
+    )
+    self_introduction = forms.CharField(
+        max_length=1000,
+        required=False,
+        widget=forms.Textarea(attrs={
+            'class': 'form-control',
+            'placeholder': '自己紹介文を入力してください（1000文字以内）'
+        })
+    )
+    self_image = forms.ImageField(
+        required=False,
+        widget=forms.FileInput(attrs={'class': 'form-control-file'})
+    )
+
+    class Meta:
+        model = User
+        fields = ('email', 'username', 'job_title', 'self_introduction', 'self_image')
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field in self.fields.values():
+            field.widget.attrs['class'] = 'form-control'
+            if field.required:
+                field.widget.attrs['required'] = 'required'
+            field.widget.attrs['placeholder'] = field.label  # placeholderにフィールドのラベルを入れる
 
 '''パスワード変更フォーム'''
 class MyPasswordChangeForm(PasswordChangeForm):
